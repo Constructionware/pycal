543876beafc6daaed98fc1dd4fd127a0bedc4739
CentryPlan <centryplan@outlook.com>
2020-08-21 11:37:14 -0500

    renamed:    pycal/MANIFEST.in -> MANIFEST.in 	renamed:    pycal/pycal/__init__.py -> pycal/__init__.py 	renamed:    pycal/pycal/construction.py -> pycal/construction.py 	renamed:    pycal/pycal/functions.py -> pycal/functions.py 	renamed:    pycal/pycal/library.py -> pycal/library.py 	renamed:    pycal/pycal/materials.py -> pycal/materials.py 	renamed:    pycal/pycal/utility.py -> pycal/utility.py 	renamed:    pycal/setup.py -> setup.py 	renamed:    pycal/tests/test.py -> tests/test.py

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from setuptools import find_packages, setup
 setup(
     name='pycal',
     author='csware',
-    author_email='csware.ware@gmail.com",
-    description="A Builder's Companion."
+    author_email="csware.ware@gmail.com",
+    description="A Builder's Companion.",
     keywords="calculate calculator build construction",
     url="https://csware.pythonanywhere.com/pycal",
     project_urls={
-        "Bug Tracker": "https://github.com/Constructionware/pycal.bugs/",
-        "Documentation": "https://github.com/Constructionware/pycal/readme.md/",
+        "Bug Tracker": "https://github.com/Constructionware/pycal/issues",
+        "Documentation": "https://github.com/Constructionware/pycal/blob/master/README.md",
         "Source Code": "https://github.com/Constructionware/pycal.git/",
+        "License": "https://github.com/Constructionware/pycal/blob/master/LICENSE",
     },
     version='0.1.0',
     packages=find_packages(),
